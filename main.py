f2fbeb2340e02ee6cfd57e3b19090b56445266e0
makostadima <kostadima@gmail.com>
2025-04-30 17:46:23 +0000

    init operator

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ tercenCtx = ctx.TercenContext()
 
 df = (
     tercenCtx
-    .select(['.y', '.ci', '.ri'], df_lib="pandas")
+    .select(['.y', '.x', '.ci', '.ri'], df_lib="pandas")
     .groupby(['.ci','.ri'], as_index=False)
-    .mean()
-    .rename(columns={".y":"mean"})
+    .max()
+    .rename(columns={".y":"y_max", ".x":"x_max"})
     .astype({".ci": np.int32, ".ri": np.int32})
 )
 
